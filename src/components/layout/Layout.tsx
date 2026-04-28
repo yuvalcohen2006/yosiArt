8e87d614c6f7bc2a30af69183a72f3aacb5a0853
yuval cohen <yuval.cohen006@gmail.com>
2026-04-28 09:30:20 +0300

    Remove CursorGlow per feedback

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,17 @@
 import Header from './Header';
 import Footer from './Footer';
 import ParticleField from '../fx/ParticleField';
-import CursorGlow from '../fx/CursorGlow';
 import AnimatedOutlet from '../fx/AnimatedOutlet';
 
 /**
  * Root layout. Sticky header, page content via AnimatedOutlet (cross-fade
  * route transitions), footer below. ParticleField sits behind everything
- * via -z-10; CursorGlow rides above content but is pointer-events: none
- * so it never intercepts clicks.
+ * via -z-10.
  */
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <ParticleField />
-      <CursorGlow />
       <Header />
       <main className="flex-1">
         <AnimatedOutlet />
